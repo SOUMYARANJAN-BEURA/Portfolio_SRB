3ea1f34e6f62b10303caed704de08b7a7945970e
Soumyaranjan-Beura <soumyaranjanbeura99@gmail.com>
2025-03-12 00:52:58 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings: ["Full Stack Development", "Python Development" , "Frontend Development" , "SQL/ NoSQL/ AWS/ LINUX"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backdelay: 1000,
+    loop: true
+});
